752ebe74459a437cc0946fc5fadb7f18773de6c9
fedor-cience <89411060+fedor-cience@users.noreply.github.com>
2023-09-20 17:16:56 +0300

    Added Loading Spinner Styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 
 gulp.task('minify-css', () => {
-  return gulp.src('source/styles.css')
+  return gulp.src('source/**.css')
     .pipe(cleanCSS())
     .pipe(concat('styles.min.css'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-js', () => {
-  return gulp.src('source/script.js')
+  return gulp.src('source/**.js')
     .pipe(uglify())
     .pipe(concat('script.min.js'))
     .pipe(gulp.dest('dist'));
