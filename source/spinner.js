de21167846b82890f9ec47e2d4c101ee58ed4aa6
fedor-cience <89411060+fedor-cience@users.noreply.github.com>
2023-09-20 17:34:42 +0300

    Added Loading Spinner Script

---
+++ source/spinner.js
@@ -0,0 +1,11 @@
+const loadingScreen = document.getElementById('loadingScreen');
+
+loadingScreen.addEventListener('transitionend', function() {
+    if (loadingScreen.style.opacity === '0') {
+        loadingScreen.style.display = 'none';
+    }
+});
+
+setTimeout(() => {
+    loadingScreen.style.opacity = '0';
+}, 500);
