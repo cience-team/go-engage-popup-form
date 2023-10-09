202d162b09423e9759f0476301dde0192859bffb
fedor-cience <89411060+fedor-cience@users.noreply.github.com>
2023-10-09 21:38:12 +0300

    Fixed Loading screen Exception

--- source/spinner.js
+++ source/spinner.js
@@ -1,11 +1,13 @@
 const loadingScreen = document.getElementById('loadingScreen');
 
-loadingScreen.addEventListener('transitionend', function() {
-    if (loadingScreen.style.opacity === '0') {
-        loadingScreen.style.display = 'none';
-    }
-});
-
-setTimeout(() => {
-    loadingScreen.style.opacity = '0';
-}, 500);
+if(loadingScreen) {
+    loadingScreen.addEventListener('transitionend', function() {
+        if (loadingScreen.style.opacity === '0') {
+            loadingScreen.style.display = 'none';
+        }
+    });
+    
+    setTimeout(() => {
+        loadingScreen.style.opacity = '0';
+    }, 500);
+}
